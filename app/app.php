f150a0bfa5dfadd8c94b9498baba183fc024c2ca
Joseph Karasek and matt knutson <joseph.karasek@gmail.com>
2016-02-09 09:22:18 -0800

    add page /view_rectangle that takes form data

--- app/app.php
+++ app/app.php
@@ -36,7 +36,13 @@
     });
 
     $app->get("/view_rectangle", function() {
-        return "You're starting to get it!";
+        $my_rectangle = new Rectangle($_GET['length'], $_GET['width']);
+        $area = $my_rectangle->getArea();
+        if ($my_rectangle->isSquare()) {
+            return "<h1>Congratulations! You made a square! Its area is $area.</h1>";
+        } else {
+            return "<h1>Sorry! This isn't a square. Its area is $area.</h1>";
+        }
     });
 
 

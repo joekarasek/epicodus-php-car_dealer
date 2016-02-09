296df5b7fe270983e167a7fe3e6b0be77f2e89a6
Joseph Karasek and matt knutson <joseph.karasek@gmail.com>
2016-02-09 09:11:12 -0800

    add form for rectangle webapp

---
+++ app/app.php
@@ -0,0 +1,44 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Rectangle.php";
+
+    $app = new Silex\Application();
+
+    $app->get("/", function() {
+        return "Home";
+    });
+
+    $app->get("/new_rectangle", function() {
+    return "<!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <title>Make a rectangle!</title>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Geometry Checker</h1>
+                <p>Enter the dimensions of your rectangle to see if it's a square.</p>
+                <form action='/view_rectangle'>
+                    <div class='form-group'>
+                      <label for='length'>Enter the length:</label>
+                      <input id='length' name='length' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                      <label for='width'>Enter the width:</label>
+                      <input id='width' name='width' class='form-control' type='number'>
+                    </div>
+                    <button type='submit' class='btn-success'>Create</button>
+                </form>
+            </div>
+        </body>
+        </html>";
+    });
+
+    $app->get("/view_rectangle", function() {
+        return "You're starting to get it!";
+    });
+
+
+    return $app;
+?>
